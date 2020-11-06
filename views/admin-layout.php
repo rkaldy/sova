a8b63e91198c51165c27fc775bbfbcadec78bc5b
Robert-Antonio <robert@antonio.cz>
2020-11-06 18:39:36 +0100

    oprava chyby v jsgrid při společném použití se select-pure

--- views/admin-layout.php
+++ views/admin-layout.php
@@ -31,7 +31,6 @@ if ($superuser) {
 <?php } else { ?>
     <link rel="stylesheet" href="https://unpkg.com/jquery-datetimepicker@2.5.21/build/jquery.datetimepicker.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid-theme.min.css">
 <?php } ?>
     <link rel="stylesheet" href="../static/selectpure.css">
     <link rel="stylesheet" href="../static/sova.css">
@@ -43,7 +42,6 @@ if ($superuser) {
 <?php if (DEVELOPMENT) { ?>
     <script src="../node_modules/jquery/dist/jquery.js"></script>
     <script src="../node_modules/jquery-datetimepicker/build/jquery.datetimepicker.full.js"></script>
-	<!--script src="../node_modules/php-date-formatter/js/php-date-formatter.js"></script-->
 	<script src="../node_modules/select-pure/dist/bundle.min.js"></script>
     <script src="../node_modules/jsgrid/dist/jsgrid.js"></script>
   <?php if ($action == "graph") { ?>
@@ -53,9 +51,9 @@ if ($superuser) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.full.min.js"></script>
 	<script src="https://unpkg.com/select-pure@0.6.1/dist/bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.js"></script>
+    <script src="../static/jsgrid.min.js"></script>
   <?php if ($action == "graph") { ?>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/vis/4.21.0/vis-network.min.js"></script>
+	<script src="https://unpkg.com/vis-network@8.5.2/dist/vis-network.min.js"></script>
   <?php } ?>
 <?php } ?>
     <script src="../static/jsgrid.custom.js"></script>
