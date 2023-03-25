4cf998f97ae5ae5304eb1fcde92634479955a1dd
Robert-Antonio <robert@antonio.cz>
2023-03-25 21:21:02 +0100

    Nulová cena imunity = imunitu nelze v dané hře koupit

--- src/Model/Hint.php
+++ src/Model/Hint.php
@@ -120,8 +120,10 @@ class Hint extends ModelBase {
 
 	public function imunityStatus(int $ccodeCount) {
 		$price = Settings::get("imunityCCodes");
-        $available = false;
-        if ($this->repo->haveImunity(Team::current())) {
+		$available = false;
+		if ($price == 0) {
+			$ret = new Text("");
+        } else if ($this->repo->haveImunity(Team::current())) {
             $ret = new Text("imunity.already");
         } else if ($ccodeCount < $price) {
             $ret = new Text("imunity.insufficient");
