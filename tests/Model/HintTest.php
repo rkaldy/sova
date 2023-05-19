1714ad4bf6d59f8bf0d1f5c70ce3400fbdf4e52d
Robert Antonio <robert@antonio.cz>
2023-05-19 08:46:42 +0200

    drobná změna v textech při prodeji céček

--- tests/Model/HintTest.php
+++ tests/Model/HintTest.php
@@ -145,12 +145,12 @@ class HintTest extends GameTestBase {
 		$this->ccode->add(2);
 
 		$resp = $this->hint->sellCCode(1);
-		$this->assertEquals(new Text("ccode.sell.add", 5, 5), $resp);
+		$this->assertEquals(new Text("ccode.sell.add", 5), $resp);
 		$this->assertEquals(5, $team->points());
         $this->assertEquals(1, $this->ccode->unusedCount());
 
 		$resp = $this->hint->sellCCode(-1);
-		$this->assertEquals(new Text("ccode.sell.sub", 5, 0), $resp);
+		$this->assertEquals(new Text("ccode.sell.sub", 5), $resp);
 		$this->assertEquals(0, $team->points());
         $this->assertEquals(0, $this->ccode->unusedCount());
 
